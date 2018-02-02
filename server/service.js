d55872e0b4de6926c4f400e4072fa7b63d1f802d
Jonathan Vargas <jonathanv3232@gmail.com>
2018-02-02 11:38:53 -0500

    adds all dependencies to the package.json file and ignores the node_modules directory

--- server/service.js
+++ server/service.js
@@ -4,6 +4,10 @@ const express = require('express');
 const request = require('superagent');
 const service = express();
 
+//AIzaSyC9JlltA_3UMVi2NNsdC9TzpWBMcnmRW0c
+//https://maps.googleapis.com/maps/api/timezone/json?location=39.6034810,-119.6822510&timestamp=1331161200&key=YOUR_API_KEY
+
+
 service.get('/service/:location', (req, res, next) => {
   request(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.params.location}&key=${process.env.GEOCODING_API_KEY}`, function(err, response) {
     if(err) {
