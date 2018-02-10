c9b6da36cc6c2a9e69499f088a346e5a63fcce99
Jonathan Vargas <jonathanv3232@gmail.com>
2018-02-10 16:39:21 -0500

    applies the utc method to the unix timestamp in order to calculate an accurate time for any location

--- server/service.js
+++ server/service.js
@@ -6,26 +6,39 @@ const moment = require('moment');
 const service = express();
 
 service.get('/service/:location', (req, res, next) => {
-  request(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.params.location}&key=${process.env.GEOCODING_API_KEY}`, function(err, response) {
-    if(err) {
-      console.log(err);
-      return res.sendStatus(500);
-    }
-    const location = response.body.results[0].geometry.location;
-    const timestamp = +moment().format('X');
-    console.log(timestamp)
-    request(`https://maps.googleapis.com/maps/api/timezone/json?location=${location.lat},${location.lng}&timestamp=${timestamp}&key=${process.env.TIMEZONE_API_TOKEN}`, function(err, response) {
-      if(err) {
+  request(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${req.params.location}&key=${
+      process.env.GEOCODING_API_KEY
+    }`,
+    function(err, response) {
+      if (err) {
         console.log(err);
         return res.sendStatus(500);
       }
-      const result = response.body;
-      console.log(result);
-      const theTime = moment.unix(timestamp + result.dstOffset + result.rawOffset).format('hh:mm:ss a');
-      console.log(theTime);
-      res.json({result: theTime});
-    })
-  });
+      const location = response.body.results[0].geometry.location;
+      const timestamp = +moment().format('X');
+      console.log(timestamp);
+      request(
+        `https://maps.googleapis.com/maps/api/timezone/json?location=${location.lat},${
+          location.lng
+        }&timestamp=${timestamp}&key=${process.env.TIMEZONE_API_TOKEN}`,
+        function(err, response) {
+          if (err) {
+            console.log(err);
+            return res.sendStatus(500);
+          }
+          const result = response.body;
+          console.log(result);
+          const theTime = moment
+            .unix(timestamp + result.dstOffset + result.rawOffset)
+            .utc()
+            .format('hh:mm:ss a');
+          console.log(theTime);
+          res.json({ result: theTime });
+        },
+      );
+    },
+  );
 });
 
 module.exports = service;
