53decc31d054f0e5c0cf7874b2e0becf25b4e8b1
Jonathan Vargas <jonathanv3232@gmail.com>
2018-02-10 14:02:06 -0500

    announces itself as a service to the main application

--- bin/run.js
+++ bin/run.js
@@ -1,11 +1,33 @@
-'use strict'
+'use strict';
 
-require('dotenv').config({path: '../../.env'});
-
+require('dotenv').config({ path: '../../.env' });
+const request = require('superagent');
 const service = require('../server/service');
 const http = require('http');
 
 const server = http.createServer(service);
 
-server.listen(3010);
-server.on('listening', () => console.log(`The server is listening on port ${server.address().port} on ${service.get('env')} mode`))
+server.listen();
+server.on('listening', () => {
+  console.log(
+    `The server is listening on port ${server.address().port} on ${service.get(
+      'env',
+    )} mode`,
+  );
+  const announce = () => {
+    request.put(
+      `http://localhost:3000/service/time/${server.address().port}`,
+      (err, res) => {
+        if (err) {
+          console.log(err);
+          console.log('There was an error connecting to the main application');
+        } else {
+          console.log(res.body);
+        }
+      },
+    );
+  };
+
+  announce();
+  setInterval(announce, 15 * 1000);
+});
